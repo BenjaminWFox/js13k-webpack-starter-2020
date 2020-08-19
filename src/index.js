4e65ef1219fe0833778a1a9109b721a7ea47a05b
Ben Fox <BenjaminWFox@gmail.com>
2020-08-19 14:26:55 -0700

    Add minimal example of importing image for use with Kontra

--- src/index.js
+++ src/index.js
@@ -1,33 +1,64 @@
-import './main.scss'
-import gameImage from './assets/images/sprites/game-image.png'
+import { init, Sprite, GameLoop } from 'kontra'
+import rawSpritePNG from './assets/images/sprites/game-image.png'
 
-console.log('Hello World')
+let { canvas, context } = init()
 
-const testESNext = (Math.random() > .5 ? 'ES2020Working' : null)
-const testESNext2 = {
-  child: {
-    node: null,
+let mySprite
+
+/**
+ * non-image, drawn sprite
+ */
+let sprite = Sprite({
+  x: 100,        // starting x,y position of the sprite
+  y: 80,
+  color: 'red',  // fill color of the sprite rectangle
+  width: 20,     // width and height of the sprite rectangle
+  height: 40,
+  dx: 2          // move the sprite 2px to the right every frame
+})
+
+/**
+ * Create HTMLImageElement to hold the imported rawSpritePNG
+ */
+const spriteImage = new Image()
+
+// assign PNG to image src
+spriteImage.src = rawSpritePNG
+
+// Create kontra sprite when image successfully loads
+spriteImage.onload = function() {
+  mySprite = Sprite({
+    x: 100,
+    y: 100,
+    anchor: { x: 0.5, y: 0.5 },
+    dx: 1,
+
+    // required for an image sprite
+    image: spriteImage
+  })
+
+  mySprite.render()
+}
+
+// Use `mySprite` as needed in Kontra
+let loop = GameLoop({  // create the main game loop
+  update: function() { // update the game state
+    sprite.update()
+    mySprite.update()
+
+    // wrap the sprites position when it reaches
+    // the edge of the screen
+    if (sprite.x > canvas.width) {
+      sprite.x = -sprite.width
+    }
+    if (mySprite.x > canvas.width) {
+      mySprite.x = -mySprite.width
+    }
   },
-}
+  render: function() { // render the game state
+    sprite.render()
+    mySprite.render()
+  }
+})
 
-const method = () => {
-  console.log('Hello Method')
-}
-
-method()
-
-console.log(testESNext ?? 'ES2020 Is Working')
-console.log(testESNext2?.child?.node?.other)
-try {
-  console.log(testESNext2?.child?.node.other)
-}
-catch (e) {
-  console.log('Caught the error', e.toString())
-}
-
-// Using an imported image
-const imgEl = document.getElementById('jsImage')
-const image = document.createElement('img')
-
-image.src = gameImage
-imgEl.appendChild(image)
+loop.start()    // start the game
